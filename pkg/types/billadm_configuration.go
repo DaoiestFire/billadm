565066ebe84fb71bc5fae2d9869678138f736028
ljw <liujingwei1997@outlook.com>
2023-02-26 20:06:04 +0800

    config

---
+++ pkg/types/billadm_configuration.go
@@ -0,0 +1,16 @@
+package types
+
+import "k8s.io/apimachinery/pkg/util/sets"
+
+type BilladmConfiguration struct {
+	BackupDirList sets.String `json:"backup_dir_list"`
+	BillDataDir   string      `json:"bill_data_dir"`
+
+	Bills map[string]*Bill `json:"bills"`
+
+	CurrentBillName string `json:"current_bill_name"`
+
+	// 2006-01-02-15:04:05
+	CreationTime   string `json:"creation_time"`
+	LastModifyTime string `json:"last_modify_time"`
+}
