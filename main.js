e1698c2f8b6826cbec509228568544b691f82e22
ljw <liujingwei1997@outlook.com>
2024-06-17 03:48:42 +0800

    增加electron编译包

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const path = require("path");
 function createWindow() {
     const win = new BrowserWindow({
         width: 1200,
-        height: 900,
+        height: 1000,
         icon: 'public/Favicon.ico',
         frame: false,
         webPreferences: {
