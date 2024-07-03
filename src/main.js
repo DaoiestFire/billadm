bbdd3d17715b617fdce39e6201623b35acce7d7c
ljw <liujingwei1997@outlook.com>
2024-07-04 00:17:35 +0800

    基于electron-vite更改项目

--- src/main.js
+++ src/main.js
@@ -1,19 +1,57 @@
-import 'normalize.css/normalize.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import './elementplus.css'
-import { createPinia } from 'pinia'
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('node:path');
 
-const app = createApp(App)
-const pinia=createPinia()
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
+if (require('electron-squirrel-startup')) {
+  app.quit();
 }
 
-app.use(ElementPlus)
-app.use(pinia)
-app.mount('#app')
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 1300,
+    height: 900,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  }
+
+  ipcMain.on("window-close", () => {
+    mainWindow.close()
+  })
+
+  ipcMain.on("window-maximize", () => {
+    if (mainWindow.isMaximized()) {
+      mainWindow.unmaximize()
+    } else {
+      mainWindow.maximize()
+    }
+  })
+
+  ipcMain.on("window-minimize", () => {
+    mainWindow.minimize()
+  })
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
---
+++ src/renderer.js
@@ -0,0 +1,19 @@
+import 'normalize.css/normalize.css'
+import 'element-plus/dist/index.css'
+import './elementplus.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import App from './App.vue'
+
+const app = createApp(App)
+const pinia = createPinia()
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.use(ElementPlus)
+app.use(pinia)
+app.mount('#app')
