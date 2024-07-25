72d140e4c6874aff8e0e709d6de5be5169ad74d6
ljw <liujingwei1997@outlook.com>
2024-07-25 23:55:59 +0800

    config

---
+++ src/config/time_shortcuts.js
@@ -0,0 +1,43 @@
+import {
+    getLastMonthDate,
+    getLastWeekData,
+    getLastYearDate,
+    getThisMonthData,
+    getThisWeekData,
+    getThisYearDate
+} from '../utils/timeutils';
+
+export const shortcuts = [
+    {
+        text: '上周',
+        value: getLastWeekData(),
+    },
+    {
+        text: '上个月',
+        value: getLastMonthDate(),
+    },
+    {
+        text: '去年',
+        value: getLastYearDate(),
+    },
+    {
+        text: '本周',
+        value: getThisWeekData(),
+    },
+    {
+        text: '本月',
+        value: getThisMonthData(),
+    },
+    {
+        text: '今年',
+        value: getThisYearDate(),
+    },
+    {
+        text: '今天',
+        value: [],
+    },
+    {
+        text: '全部',
+        value: [null, null],
+    },
+];
